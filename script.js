aba0b915ddf5a34d70459d749e5d1b3b0ed9ad1c
Hashim271 <hashim46@live.ca>
2025-04-09 21:50:37 -0400

    Added Part 1: Understanding and Creating Objects

---
+++ script.js
@@ -0,0 +1,10 @@
+const student = {
+    Student: "Muhammad Hashim Ali",
+    Age: 23,
+    Enrolled: true,
+    Courses: ["Math", " Music", " French", " Physics", " Programming I", " Chemistry"],
+
+    displayBasicInfo: function () {
+        console.log(`Enrollement Status: ${this.Enrolled},\nCourses: ${this.Courses}`);
+    }
+};

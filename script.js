c796b7919a203f25ceb33c81889302c3bd760b04
Hashim271 <hashim46@live.ca>
2025-04-09 21:57:15 -0400

    Added JSON stringify

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1: Understanding and Creating Objects
 const student = {
     Student: "Muhammad Hashim Ali",
     Age: 23,
@@ -8,3 +9,13 @@ const student = {
         console.log(`Enrollement Status: ${this.Enrolled},\nCourses: ${this.Courses}`);
     }
 };
+
+console.log("\n") //LOG part 1
+console.log("Student: " + student.Student + "\n" + "Age: " + student.Age);
+student.displayBasicInfo();
+console.log("\n");
+
+// Part 2: Working with JSON
+const studentJSON = JSON.stringify(student);
+console.log("JSON String");
+console.log
